3f2da9317cecfede7f51fe9e9b616f7ada9c9d92
ShristiAnjana-bit <Shristip263@gmail.com>
2026-05-23 17:39:50 +0530

    Add budget tracker and guessing game source code files

--- budget.py
+++ budget.py
@@ -1,8 +1,18 @@
+import json
 #line 1: Get the starting income and convert it to a decimal number(float)
 income = float(input("Enter your monthly income:"))
 
 #line 2: create an empty dictionary(like a java hashmap) to store "expense Name" :Amount
-expenses = {}
+#expenses = {} REPLACEING this with
+#Try to open and read the existing file
+try:
+    with open("expenses.json","r") as file: #'r' means read mode
+         expenses = json.load(file) #load the json data back into a dictionary
+    print("Loaded your saved expenses successfully!")
+except FileNotFoundError:
+    #if the file doesn't exist yet,that's fine!Just start with empty dictionary:
+    print("No saved data found.Starting a fresh budget.")
+    expenses ={}
 
 #line 3: start an infinite loop that keep asking for expenses
 while True:
@@ -39,5 +49,12 @@ print("------------------------")
 print(f"Total Income:   ${income}")
 print(f"Total Expenses: ${total_expenses}")
 print(f"Net Savings:    ${balance}")
+
+#'with open' safely open a file and automatically closes it when done
+#'w' stand for write done
+with open ("expenses.json","w") as file:
+    #this takes your 'expenses' dictionary and write it cleanly into the file
+    json.dump(expenses,file, indent=4)
+print("\n Data successfully saved to expenses.json!")
     
     
