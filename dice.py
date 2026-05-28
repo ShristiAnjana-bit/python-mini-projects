8f74529ae55a3a3a432feff8a36807923dc2e762
ShristiAnjana-bit <Shristip263@gmail.com>
2026-05-28 13:05:40 +0530

    add dice rolling simulator

---
+++ dice.py
@@ -0,0 +1,11 @@
+import random
+while True:
+     number = random.randint(1,6)
+     print(number)
+     again = input("Roll again? yes/no: ")
+     if again == "yes":
+        continue
+     else:
+          break  
+    
+
