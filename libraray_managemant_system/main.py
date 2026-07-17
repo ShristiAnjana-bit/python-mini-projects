b340b5ddfad994d5942dc8d225be35917c27421b
ShristiAnjana-bit <Shristip263@gmail.com>
2026-07-17 10:23:50 +0530

    Complete Library Management System v1.0

--- libraray_managemant_system/main.py
+++ libraray_managemant_system/main.py
@@ -26,4 +26,25 @@ while True:
         else:
             print("No books available.")
 
-    
+    elif choice == "3":
+        book_name = input("Enter book name:")
+
+        if library.search_book(book_name):
+                print("Book Found")
+        else:
+            print("Book Not Found")
+
+    elif choice == "4":
+        book_name = input("Enter book name: ")
+
+        if library.remove_book(book_name):
+            print("Book removed successfully.")
+        else:
+            print("Book not found.")
+
+    elif choice == "5":
+        print("Thank you for using Library Managemant System!")
+        break
+
+    else:
+        print("Invalid choice! Please try again.")
