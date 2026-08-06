bfc9b578b9c24f66adf3ee92fbb757283cecae56
ShristiAnjana-bit <Shristip263@gmail.com>
2026-08-06 11:23:20 +0530

    feat: add JSON-based book storage and search functionality

--- libraray_management_system_v2/library.py
+++ libraray_management_system_v2/library.py
@@ -10,8 +10,41 @@ class Library:
             with open("books.json","r") as file:
                 self.books = json.load(file)
         except FileNotFoundError:
-            pass
+            self.books = []
+        except json.JSONDecodeError:
+            self.books = []
+            
 
     def save_books(self):
         with open("books.json","w") as file:
             json.dump(self.books,file)
+
+    def add_book(self,title,author,edition,publisher):
+        #validation
+        for value in (title,author,edition,publisher):
+            if not value.strip():
+                return False
+
+        #Create dictionary
+        book = {
+            "title": title,
+            "author": author,
+            "edition": edition,
+            "publisher": publisher
+        }
+
+        #add book
+        self.books.append(book)
+
+        #Save to JSON
+        self.save_books()
+
+        #success
+        return True
+
+    def search_book(self,title):
+        for book in self.books:
+            if book["title"].lower() == title.lower():
+               return book
+        return None 
+
---
+++ libraray_management_system_v2/main.py
@@ -0,0 +1,25 @@
+from library import Library
+
+library = Library()
+
+while True:
+    print("\n===== Library Management System =====")
+    print("1. Add Book")
+    print("2.View Books")
+    print("3.Search Book")
+    print("4.Remove Book")
+    print("5.Update Book")
+    print("6.Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        title = input("Enter title:")
+        author = input("Enter author:")
+        edition = input("Enter edition:")
+        publisher = input("Enter publisher:")
+
+        if library.add_book(title,author,edition,publisher):
+            print("Book added successfully.")
+        else:
+             print("Please fill in all the fields.")
